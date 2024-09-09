4266f627a164b08e4ddcf3b7f0e5782101ec6f39
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-08 21:37:43 -0400

    Added point-rec test exercise

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
 #include "raylib.h"
 #include "Math.h"
 
+bool PointInRec(Vector2 point, Rectangle rec)
+{
+    // Fill in this function to determine if a point is inside a rectangle
+    // A point is inside a rectangle if:
+    // point x > rec x && point x < rec x + rec width &&
+    // point y > rec y && point y < rec y + rec height
+    // For more information, see https://www.jeffreythompson.org/collision-detection/point-rect.php
+    return false;
+}
+
 int main()
 {
+    Rectangle rec;
+    rec.x = 200;
+    rec.y = 200;
+    rec.width = 160;
+    rec.height = 80;
+
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        Vector2 mouse = GetMousePosition();
+        Color color = PointInRec(mouse, rec) ? RED : GREEN;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 10, 10, 20, GRAY);
+        DrawCircleV(mouse, 3.0f, color);
+        DrawRectangleRec(rec, color);
         EndDrawing();
     }
 
