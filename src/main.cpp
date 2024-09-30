f07dee25773a88ed7746b3fd22e41b9034350baf
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-30 14:28:56 -0400

    Rendered a single line

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,20 @@ int main()
 {
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
+    float w = GetScreenWidth();
+    float h = GetScreenHeight();
+    float tc = 10.0f;
+
     while (!WindowShouldClose())
     {
+        // Draw a line from the top-left to the top-right of our screen!
+        Vector2 A = { 0.0f, 0.0f };
+        Vector2 B = { w, 0.0f };
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
         DrawText("Hello World!", 10, 10, 20, GRAY);
+        DrawLineEx(A, B, tc, VIOLET);
         EndDrawing();
     }
 
